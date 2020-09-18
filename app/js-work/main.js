58ca8c8f8ce7712859e95f624b36e1870dd9f8fa
Evi-ava <40437099+Evi-ava@users.noreply.github.com>
2020-09-19 00:16:16 +0300

    Коммит с готовым слайдером

--- app/js-work/main.js
+++ app/js-work/main.js
@@ -1,7 +1,19 @@
+
 var $page = $('html, body');
 $('a[href*="#"]').click(function() {
     $page.animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 400);
     return false;
+});
+
+$('.slider').slick({
+    infinite: true,
+    arrows: false,
+    lazyLoad: 'ondemand',
+    autoplay: true,
+    autoplaySpeed: 4000,
+    speed: 700,
+    fade: true,
+    cssEase: 'linear'
 });
--- gulpfile.js
+++ gulpfile.js
@@ -65,8 +65,8 @@ gulp.task('scss', function() {
 
 gulp.task('libs-js', function() {
 	return gulp.src(
-		'node_modules/jquery/dist/jquery.js',
-		//'node_modules/slick-carousel/slick/slick.js',
+		['node_modules/jquery/dist/jquery.js',
+		'node_modules/slick-carousel/slick/slick.min.js']
 		//'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
 	)
 	.pipe(concat('libs.min.js'))
