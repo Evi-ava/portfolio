441a49560867a1cc20745452bf657d396df54a99
Evi-ava <40437099+Evi-ava@users.noreply.github.com>
2020-09-19 00:34:42 +0300

    Добавил фон для слайдов

--- app/js-work/main.js
+++ app/js-work/main.js
@@ -1,19 +1,23 @@
+$(document).ready(function(){
+    var $page = $('html, body');
 
-var $page = $('html, body');
-$('a[href*="#"]').click(function() {
-    $page.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 400);
-    return false;
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 400);
+        return false;
+    });
+
+    $('.slider').slick({
+        infinite: true,
+        arrows: false,
+        lazyLoad: 'ondemand',
+        autoplay: true,
+        autoplaySpeed: 4000,
+        speed: 700,
+        fade: true,
+        cssEase: 'linear'
+    });
 });
 
-$('.slider').slick({
-    infinite: true,
-    arrows: false,
-    lazyLoad: 'ondemand',
-    autoplay: true,
-    autoplaySpeed: 4000,
-    speed: 700,
-    fade: true,
-    cssEase: 'linear'
-});
+
