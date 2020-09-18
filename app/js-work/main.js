3c0417f33dd767bde1e147d02a312fe4f499f94b
Evi-ava <40437099+Evi-ava@users.noreply.github.com>
2020-09-18 23:05:25 +0300

    Прокрутка к якорю

--- app/js-work/main.js
+++ app/js-work/main.js
@@ -1,16 +1,7 @@
-// let anchors = document.querySelectorAll('a[href*="#"]');
-
-// for (let anchor of anchors) {
-//   anchor.addEventListener('click', function (e) {
-//     e.preventDefault()
-    
-//     const blockID = anchor.getAttribute('href').substr(1)
-    
-//     document.getElementById(blockID).scrollIntoView({
-//       behavior: 'smooth',
-//       block: 'start'
-//     })
-//   })
-// }
-
-console.log(1)
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 400);
+    return false;
+});
